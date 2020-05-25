31dffe78e71ee98e76c9835cec17ba8ebdc3e18c
Aleksander Bloch <alekbloch21@gmail.com>
2020-05-25 22:25:12 +0200

    Add handling of PUSH and OUTPUT

--- komp.c
+++ komp.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MULTIPLIER 3
+#define DIVISOR 2
 
 /*
 VIRTUAL MACHINE COMPILER
@@ -28,7 +32,7 @@ enum instructions {
 typedef struct inst_template {
     enum instructions type;
     int address;
-    int stack_num;
+    int stack;
 }inst_t;
 
 /*
@@ -54,5 +58,52 @@ int main() {
 
 inst_t** precompile(int* inst_num, int* choice_add_num,
 int* proc_ad, int* main_ad) {
-    ;
+    // capacity of inst_arr
+    int capacity = 1;
+    // ith item, token value
+    int i, token;
+    // flag to indicate the source of bits
+    int use_stack, stack_num;
+
+
+    // initializing the inst_arr with jump to main
+    inst_t* inst_arr = (inst_t*)malloc(sizeof(inst_t));
+    inst_arr[0].type = JUMP;
+
+
+    // translating tokens into instructions in vm code
+    for(int i = 1; (token = getchar()) != EOF; ++i) {
+        // make sure there is enough space for next inst
+        if(i == capacity) {
+            capacity = 1 + capacity * MULTIPLIER / DIVISOR;
+            inst_arr = realloc(inst_arr, capacity * sizeof(inst_t));
+        }
+        // use stack mode
+        if(token >= 'a' && token <= 'z') {
+            use_stack = 1;
+            stack_num = token - 'a';
+        }
+        // stdin mode
+        else if(token == '$') {
+            use_stack = 0;
+        }
+        // processing tokens according to mode
+        else if(use_stack) {
+            if(token == '-')
+                inst_arr[i].type = PUSH_0;
+            else
+                inst_arr[i].type = PUSH_1;
+
+            inst_arr[i].stack = stack_num;
+        }
+        else {
+            if(token == '-')
+                inst_arr[i].type = OUTPUT_0;
+            else
+                inst_arr[i].type = OUTPUT_1;
+            
+            inst_arr[i].stack = stack_num;
+        }
+    }
+    *inst_num = i;
 }
